6119fbfd20541238353ecd9eb4e573eccf32af29
minh187pm13970 <minh.187pm13970@vanlanguni.vn>
2023-09-24 20:29:51 +0700

    create feature addInfo / deleteInfo / returnInfo

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,12 +9,38 @@ const Animal = function (name, color, weight) {
   let nameAnimal = name;
   let colorAnimal = color;
   let weightAnimal = weight;
-
+    const infoArr = [];
   return {
+    addInfo(value) {
+        if(typeof value === 'string') infoArr.push(value);
+    },
+
     showInfo() {
       return `name: ${nameAnimal}, color: ${colorAnimal}, weight: ${weightAnimal}`;
     },
-  };
+    
+    deleteInfo(value) {
+        const index = infoArr.indexOf(value);
+        if(index !== -1) {
+            infoArr.splice(index, 1);
+        } else {
+            console.log('khong co gia tri nay!')
+        }
+    },
+
+    returnInfo() {
+        return infoArr;
+    },
+    }
 };
 
-console.log(Animal("dog", "black", 100).showInfo());
+const pet = Animal('dog','black',100);
+pet.addInfo('chihuahua')
+pet.addInfo('long tail')
+pet.addInfo('short')
+pet.addInfo('japan dog')
+console.log(pet.returnInfo())
+
+pet.deleteInfo('japan dog')
+console.log(pet.returnInfo())
+console.log(pet.showInfo())
